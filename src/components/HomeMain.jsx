c13f7916f92e20e55f0b50025df76e5bc0207648
KacNyc <nyczak3@gmail.com>
2024-07-31 18:22:10 +0200

    Completed main page with About us section

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -1,4 +1,32 @@
 import React from 'react';
+import homeMain from './homeMain.scss';
+
+const entries = {
+    fundacje: [
+        ["Fundacja 1", "Opis 1", "Egestas, sed, tempus"],
+        ["Fundacja 2", "Opis 2", "Ut, aliquam, purus, sit, amet"],
+        ["Fundacja 3", "Opis 3", "Mi, quis, hendrerit, dolor"],
+        ["Fundacja 4", "Opis 4", "Nunc, lobortis, mattis, aliquam"],
+        ["Fundacja 5", "Opis 5", "Vestibulum, sit, amet, cursus"],
+        ["Fundacja 6", "Opis 6", "In, viverra, orci, mollis"],
+        ["Fundacja 7", "Opis 7", "Aenean, facilisis, mi, eget"],
+        ["Fundacja 8", "Opis 8", "Mauris, pharetra, nisl, vitae"],
+        ["Fundacja 9", "Opis 9", "Curabitur, tristique, libero, vitae"],
+    ],
+    organizacje: [
+        ["Organizacja 1", "Opis 1", "Egestas, sed, tempus"],
+        ["Organizacja 2", "Opis 2", "Ut, aliquam, purus, sit, amet"],
+        ["Organizacja 3", "Opis 3", "Mi, quis, hendrerit, dolor"],
+        ["Organizacja 4", "Opis 4", "Nunc, lobortis, mattis, aliquam"],
+        ["Organizacja 5", "Opis 5", "Vestibulum, sit, amet, cursus"],
+        ["Organizacja 6", "Opis 6", "In, viverra, orci, mollis"],
+    ],
+    zbiorki: [
+        ["Zbiórka 1", "Opis 1", "Egestas, sed, tempus"],
+        ["Zbiórka 2", "Opis 2", "Ut, aliquam, purus, sit, amet"],
+        ["Zbiórka 3", "Opis 3", "Mi, quis, hendrerit, dolor"],
+    ]
+};
 
 const HomeMain = () => {
     return (
